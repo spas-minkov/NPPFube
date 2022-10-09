28a8147aea8a2110e191f409687240358a09fb7f
s.minkov <s.minkov@tinqin.com>
2022-10-09 09:33:47 +0300

    Some formatting fixes

--- config/fube_definitions.py
+++ config/fube_definitions.py
@@ -1,22 +1,36 @@
 definitions = (
     (
         "FUBE_V2",
-        {"REC001": ["Header", "^000"], "REC002": ["Body", "^444"], "REC003": ["Footer", "^999"]},
+        {
+            "REC001": ["Header", "^000"],
+            "REC002": ["Body", "^444"],
+            "REC003": ["Footer", "^999"]
+        },
         r"^.{21}(FUBXM|FUBXE)"
     ),
     (
         "FUBE_V2_1",
-        {"REC001": ["Header", "^000"], "REC002": ["Body", "^444"], "REC003": ["Footer", "^999"]},
+        {
+            "REC001": ["Header", "^000"],
+            "REC002": ["Body", "^444"],
+            "REC003": ["Footer", "^999"]
+        },
         r"^.{21}(FUBYM|FUBYE)"
     ),
     (
         "FUBE_V2_2",
-        {"REC001": ["Header", "^000"], "REC002": ["Body", "^444"], "REC003": ["Footer", "^999"]},
+        {
+            "REC001": ["Header", "^000"],
+            "REC002": ["Body", "^444"],
+            "REC003": ["Footer", "^999"]
+        },
         r"^.{21}(FUBZM|FUBZE)"
     ),
     (
         "FUBE_V1",
-        {"REC001": ["Body", "^.{2968}$"]},
+        {
+            "REC001": ["Body", "^.{2968}$"]
+        },
         r"^.{2968}$"
     )
 )
